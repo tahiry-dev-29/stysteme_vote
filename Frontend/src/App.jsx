aefda77b59b991e13ba38cfbc31ccfdd3c79f151
Diary <diaryvatosoarakotonoelina@gmail.com>
2025-06-07 00:17:14 -1000

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-red-600">
+        React + Vite + Tailwind CSS v4
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
