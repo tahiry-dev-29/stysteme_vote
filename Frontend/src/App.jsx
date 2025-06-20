12692707993eb29a64dde764aa36ffc91596e448
Diary <diaryvatosoarakotonoelina@gmail.com>
2025-06-20 02:12:25 -1000

    Remove unused CSS and component files for a cleaner project structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css'
 
+
 function App() {
 
   return (
-    <>
-      <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-red-600">
-        React + Vite + Tailwind CSS v4
-      </h1>
-    </div>
-    </>
+    
+   <>
+   </>
   )
 }
 
